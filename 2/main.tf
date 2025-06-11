d366da1d98d5e869dbe68f16221fc5bc51bc8e2c
kyp <kyp@kmc.gr.jp>
2025-06-11 21:39:51 +0900

    サーバーを立てる

--- 2/main.tf
+++ 2/main.tf
@@ -1,12 +1,32 @@
 provider "aws" {
-    region = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0febccb66c819dac9" // ubuntu-20.04
-    instance_type = "t2.micro"
+  ami                    = "ami-0febccb66c819dac9" // ubuntu-20.04
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-    tags = {
-        Name = "terraform-example"
-    }
+  user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, Terraform" > index.html
+        nohup busybox httpd -f -p 8080 &
+    EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
