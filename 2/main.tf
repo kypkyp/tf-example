0d52cff4091c0f6a5632d3b8fc6ded56dc5177c9
kyp <kyp@kmc.gr.jp>
2025-06-11 18:26:50 +0900

    change name

--- 2/main.tf
+++ 2/main.tf
@@ -5,4 +5,8 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0febccb66c819dac9" // ubuntu-20.04
     instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-example"
+    }
 }
