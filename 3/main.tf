37912c7c4496e6a715cff5a57e6908fead798bd9
kyp <kyp@kmc.gr.jp>
2025-06-17 09:46:40 +0900

    backend s3

---
+++ 3/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  backend "s3" {
+    bucket       = "terraform-up-and-running-state-kyp"
+    key          = "global/s3/terraform.tfstate"
+    region       = "ap-northeast-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-kyp"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# デフォルトで暗号化されるので不要
+# resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+#   bucket = aws_s3_bucket.terraform_state.id
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
