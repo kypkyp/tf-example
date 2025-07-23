65ab96fdc15c2ef00f7bc646521c91c55c2b45de
kyp <kyp@kmc.gr.jp>
2025-07-23 10:39:00 +0900

    4

---
+++ 4/modules/webserver-cluster/user-data.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cat > index.html <<EOF
+<h1>Hello, Terraform</h1>
+<p>DB address: ${db_address}</p>
+<p>DB port: ${db_port}</p>
+EOF
+
+nohup busybox httpd -f -p ${server_port} &
---
+++ 4/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/webserver-cluster"
+
+  cluster_name           = "webservers-stg"
+  db_remote_state_bucket = "terraform-up-and-running-state-kyp"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+}
