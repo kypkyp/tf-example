d7c61db007da62d090c021b0cfb85b16e84418ed
kyp <kyp@kmc.gr.jp>
2025-07-23 15:53:32 +0900

    4

--- 4/modules/webserver-cluster/main.tf
+++ 4/modules/webserver-cluster/main.tf
@@ -44,13 +44,16 @@ resource "aws_autoscaling_group" "example" {
 
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
+}
 
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_lb" "example" {
@@ -92,6 +95,26 @@ resource "aws_security_group" "alb" {
     protocol    = local.any_port
     cidr_blocks = local.all_ips
   }
+}
+
+resource "aws_security_group_rule" "alb_allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = "tcp"
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "alb_allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb_target_group" "asg" {
--- 4/modules/webserver-cluster/outputs.tf
+++ 4/modules/webserver-cluster/outputs.tf
@@ -7,3 +7,8 @@ output "alb_dns_name" {
   value       = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
 }
+
+output "alb_security_group_id" {
+  value       = aws_security_group.alb.id
+  description = "The ID of the security group attached to the load balancer"
+}
