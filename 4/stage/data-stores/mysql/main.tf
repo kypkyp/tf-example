4e1f471869a88e0d177333800391687f7386d975
kyp <kyp@kmc.gr.jp>
2025-07-23 11:08:09 +0900

    db側も

--- 4/stage/data-stores/mysql/main.tf
+++ 4/stage/data-stores/mysql/main.tf
@@ -12,14 +12,9 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_db_instance" "example" {
-  identifier_prefix   = "terraform-up-and-running"
-  engine              = "mysql"
-  allocated_storage   = 10
-  instance_class      = "db.t3.micro"
-  skip_final_snapshot = true
-  db_name             = "example_database"
+module "mysql" {
+  source = "../../../modules/mysql"
 
-  username = var.db_username
-  password = var.db_password
+  db_username = var.db_username
+  db_password = var.db_password
 }
